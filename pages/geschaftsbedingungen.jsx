ded2059cf29fa381f41ded5d7bb8a819c0ddbf9d
abdo643 <abou.shehata643@yahoo.de>
2021-04-03 04:47:17 +0200

    agb fix

--- pages/geschaftsbedingungen.jsx
+++ pages/geschaftsbedingungen.jsx
@@ -5,13 +5,13 @@ export default function geschaftsbedingungen({ agb }) {
 	return <Agb agb={agb} />;
 }
 
-// import { getAGB } from '@utils/Contentful';
+import { getAGB } from '@utils/Contentful';
 export async function getStaticProps() {
-	const { readFile, writeFile } = require('fs').promises;
-	// const data = await getAGB();
+	// const { readFile, writeFile } = require('fs').promises;
 	// await writeFile('agb.json', JSON.stringify(data));
+	// const agb = await readFile('agb.json');
 
-	const agb = await readFile('agb.json');
+	const data = await getAGB();
 
 	return {
 		props: { agb: JSON.parse(agb) },
