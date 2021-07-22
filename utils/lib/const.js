143931a88d522f7b9c9eddc8b1d42ad2a94b70ef
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 15:28:06 +0200

    added back without hardcoded env vars

---
+++ utils/lib/const.js
@@ -0,0 +1,18 @@
+export const SHOPIFY_DOMAIN = process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN || '';
+export const SHOPIFY_ACCESS_TOKEN = process.env.NEXT_PUBLIC_SHOPIFY_ACCESS_TOKEN || '';
+
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || '';
+
+export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '';
+export const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN || '';
+
+//Meine Daten
+export const OLD_SHOPIFY_DOMAIN = process.env.OLD_SHOPIFY_DOMAIN;
+export const OLD_SHOPIFY_ACCESS_TOKEN = process.env.OLD_SHOPIFY_ACCESS_TOKEN;
+
+export const MY_CONTENTFUL_SPACE_ID = process.env.MY_CONTENTFUL_SPACE_ID;
+export const MY_CONTENTFUL_ACCESS_TOKEN = process.env.MY_CONTENTFUL_ACCESS_TOKEN;
+
+export const CONTENTFUL_PREVIEW_ACCESS_TOKEN = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+export const CONTENTFUL_PREVIEW_SECRET = process.env.CONTENTFUL_PREVIEW_SECRET;
