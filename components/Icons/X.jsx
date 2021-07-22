1dea07448202d5618f8e8ee22f1c385b037036bc
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-07-22 15:30:35 +0200

    a11y improvment

--- components/Icons/X.jsx
+++ components/Icons/X.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default function X({ width = '24px', color = '#fff' }) {
 	return (
-		<svg xmlns='http://www.w3.org/2000/svg' stroke={color} viewBox='0 0 32 32' width={width}>
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			aria-hidden='true'
+			focusable='false'
+			stroke={color}
+			viewBox='0 0 32 32'
+			width={width}
+		>
 			<line strokeWidth='4' x1='2' x2='30' y1='2' y2='30' />
 			<line strokeWidth='4' x1='2' x2='30' y1='30' y2='2' />
 		</svg>
